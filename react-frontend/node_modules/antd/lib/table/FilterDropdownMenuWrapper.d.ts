3621d0b99ee3b178c8d999ae918d6684de0ea59f
Cynthia <b04901152@ntu.edu.tw>
2019-11-14 00:40:18 +0800

    go

--- react-frontend/node_modules/antd/lib/table/FilterDropdownMenuWrapper.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-export interface FilterDropdownMenuWrapperProps {
-    children?: any;
-    className?: string;
-}
-declare const _default: (props: FilterDropdownMenuWrapperProps) => JSX.Element;
-export default _default;
